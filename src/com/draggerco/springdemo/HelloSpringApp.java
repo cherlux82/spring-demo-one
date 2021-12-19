8a1a4f53ba6363d74d6a6490cccb5355696520fc
cherlux82 <icc.vicente.garcia@hotmail.com>
2021-12-18 23:43:33 -0600

    Clase 27 DI por XML

---
+++ src/com/draggerco/springdemo/HelloSpringApp.java
@@ -0,0 +1,17 @@
+package com.draggerco.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		System.out.println("====================================== S P R I N G =========================================");
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		context.close();
+
+	}
+
+}
---
+++ src/com/draggerco/springdemo/KarateCoach.java
@@ -0,0 +1,10 @@
+package com.draggerco.springdemo;
+
+public class KarateCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Make some karate";
+	}
+
+}
